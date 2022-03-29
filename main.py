d2c40693e13decb35a526d706160b851117581a4
Robyn Seymour-Jones <robynfsj@gmail.com>
2022-03-29 19:29:47 +0100

    improve exception handling when getting user input

--- main.py
+++ main.py
@@ -11,15 +11,21 @@
 #   - return positive integer of type int
 
 def get_num_of_primes():
+    n = input("Enter number of primes to calculate (positive integer): ")
+
+    # Handle cases where user inputs characters other than numbers.
     try:
-        n = int(input("Number of primes to calculate: "))
-        if n < 1:
-            raise ValueError
+        int_n = int(n)
     except ValueError:
-        print("Invalid input. Please enter a positive integer.\n")
+        print("Invalid input. Cannot contain non-numeric characters.\n")
         return get_num_of_primes()
     else:
-        return n
+        # Handle cases where int is not positive.
+        if int_n >= 1:
+            return int_n
+        else:
+            print("Invalid input. Integer must be positive.\n")
+            return get_num_of_primes()
 
 
 # (2) find primes
